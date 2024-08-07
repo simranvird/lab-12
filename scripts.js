7d4c81638e83e264c36cf319e8c7669631477304
simranvird <169073573+simranvird@users.noreply.github.com>
2024-08-07 15:36:54 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggleHighlight(){
+    const elements=document.getElementsByClassName('text');
+    for(let i=0 ; i<elements.length; i++){
+        elements[i].classList.toggle('highlight');
+    }
+}
